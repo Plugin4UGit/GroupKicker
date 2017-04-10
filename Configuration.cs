fbede49404d7db1bc47168499af3057cf8621add
Plugin4UGit <Plugin4UGit@users.noreply.github.com>
2017-04-10 17:34:57 +0300

    Update Configuration.cs

--- Configuration.cs
+++ Configuration.cs
@@ -4,17 +4,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace GroupKicker
 {
     public class Configuration : IRocketPluginConfiguration
     {
+        [XmlArrayItem(ElementName = "Group")]
         public List<CSteamID> Groups;
         public void LoadDefaults()
         {
             Groups = new List<CSteamID>()
             {
-                new CSteamID(11111111111),
+                new CSteamID(103582791439889796),
             };
         }
     }
