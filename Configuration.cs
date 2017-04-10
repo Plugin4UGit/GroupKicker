9beed7654adbcf4dfdcbfb05c010758ea963b9eb
Plugin4UGit <plugin4u@outlook.com>
2017-04-10 17:31:20 +0300

    GroupKicker

---
+++ Configuration.cs
@@ -0,0 +1,21 @@
+﻿using Rocket.API;
+using Steamworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GroupKicker
+{
+    public class Configuration : IRocketPluginConfiguration
+    {
+        public List<CSteamID> Groups;
+        public void LoadDefaults()
+        {
+            Groups = new List<CSteamID>()
+            {
+                new CSteamID(11111111111),
+            };
+        }
+    }
+}
---
+++ GroupKicker.cs
@@ -0,0 +1,37 @@
+﻿using Rocket.Core.Plugins;
+using Rocket.Unturned;
+using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GroupKicker
+{
+    public class GroupKicker : RocketPlugin<Configuration>
+    {
+        public bool Added = false;
+        public GroupKicker Instance;
+        protected override void Load()
+        {
+            Instance = this;
+            base.Load();
+            U.Events.OnPlayerConnected += Events_OnPlayerConnected;
+        }
+
+        private void Events_OnPlayerConnected(Rocket.Unturned.Player.UnturnedPlayer player)
+        {
+            foreach(var id in Instance.Configuration.Instance.Groups)
+            {
+                if(id == player.SteamGroupID)
+                {
+                    Added = true;
+                }
+            }
+            if(Added == false)
+            {
+                Provider.kick(player.CSteamID, "");
+            }
+        }
+    }
+}
